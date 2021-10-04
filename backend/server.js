904289814000f671ca78676851fc40ff86535502
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-03 22:02:50 -0500

    setting up folders

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("expresss");
+require("dotenv").config();
+
+const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port $(PORT)`));
+app.listen(PORT, () => console.log(`Server running on port${PORT}`));
